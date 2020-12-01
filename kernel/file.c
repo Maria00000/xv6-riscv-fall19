0baa838378b90e3f610fd490ee19224f651952d0
Maria00000 <3381862149@qq.com>
2020-12-01 23:00:15 +0800

    Lab4

--- kernel/file.c
+++ kernel/file.c
@@ -16,7 +16,7 @@
 struct devsw devsw[NDEV];
 struct {
   struct spinlock lock;
-  struct file file[NFILE];
+  //struct file file[NFILE];
 } ftable;
 
 void
@@ -32,13 +32,22 @@ filealloc(void)
   struct file *f;
 
   acquire(&ftable.lock);
-  for(f = ftable.file; f < ftable.file + NFILE; f++){
-    if(f->ref == 0){
-      f->ref = 1;
-      release(&ftable.lock);
-      return f;
-    }
-  }
+  f=bd_malloc(sizeof(struct file));
+
+  if(f){
+    memset(f,0,sizeof(struct file));
+    f->ref=1;
+    release(&ftable.lock);
+    return f;
+  }  
+  // for(f = ftable.file; f < ftable.file + NFILE; f++){
+  //   if(f->ref == 0){
+  //     f->ref = 1;
+  //     release(&ftable.lock);
+  //     return f;
+  //   }
+  // }
+
   release(&ftable.lock);
   return 0;
 }
@@ -60,7 +69,7 @@ void
 fileclose(struct file *f)
 {
   struct file ff;
-
+ 
   acquire(&ftable.lock);
   if(f->ref < 1)
     panic("fileclose");
@@ -71,8 +80,9 @@ fileclose(struct file *f)
   ff = *f;
   f->ref = 0;
   f->type = FD_NONE;
+  bd_free(f);
   release(&ftable.lock);
-
+ 
   if(ff.type == FD_PIPE){
     pipeclose(ff.pipe, ff.writable);
   } else if(ff.type == FD_INODE || ff.type == FD_DEVICE){
